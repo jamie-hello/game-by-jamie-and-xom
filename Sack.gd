aa5fce12da92fd4a15d00d259015ab510b4997dc
jamie-hello <jamiecrom22@gmail.com>
2023-07-08 21:20:40 -0400

    added values to the board

--- Sack.gd
+++ Sack.gd
@@ -10,3 +10,18 @@ func _ready():
 func _process(delta):
 	
 	pass
+
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			$Area2D.set_position(event.position)
+			var clickzones = []
+			clickzones = $Area2D.get_overlapping_areas()
+			if clickzones.is_empty():
+				var instnc = instance.instantiate()
+				instnc.set_position(event.position)
+				add_child(instnc)
+				print("Mouse Click/Unclick at: ", event.position)
+				pass
+			
