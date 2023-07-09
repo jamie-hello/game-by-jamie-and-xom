000f1c9be0e80e0fc0a48b41d3a80d71fc15dfe0
Xom <xom@xom.io>
2023-07-09 07:22:01 -0400

    fiddle with score HUD

--- Score.gd
+++ Score.gd
@@ -1,21 +1,22 @@
 extends Control
 
+
 @onready var CurrentScore = $CurrentScore
 @onready var newscorepopup = $"newScore Popup"
+
+
 func _process(delta):
 	CurrentScore.set_text(str(get_parent().score))
-	pass
-	
-
-
 
 
 func _on_deal_zone_player_new_score(wordscore,word,isOpening):
 	if not isOpening:
 		newscorepopup.get_node("Label").set_visible(0)
+		newscorepopup.set_text(word+" +"+str(wordscore))
+	else:
+		newscorepopup.set_text(word+" ("+str(wordscore)+")")
 	newscorepopup.set_position(Vector2(-218,10))
 	newscorepopup.set_modulate(Color(1,1,1,1))
-	newscorepopup.set_text(word+" +"+str(wordscore))
 	newscorepopup.get_node("Label").set_text("(First turn, No Score)")
 	var tween = newscorepopup.create_tween()
 	tween.tween_property(newscorepopup,"position",Vector2(-186,-20),1.50)
